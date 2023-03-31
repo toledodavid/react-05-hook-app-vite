daf6dfb24f16aaa80dd3fefab452c0f20342cd9b
David Toledo <dtoledoz0207@gmail.com>
2023-03-31 11:03:35 -0600

    Create CounterApp component and inplement useState hook for a counter task

---
+++ src/01-useState/CounterApp.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+
+export const CounterApp = () => {
+
+  const [{counter1, counter2, counter3}, setCounter] = useState({counter1: 10, counter2: 20, counter3: 30});
+
+  return(
+    <>
+      <h1>Counter1: {counter1}</h1>
+      <h1>Counter2: {counter2}</h1>
+      <h1>Counter3: {counter3}</h1>
+
+      <hr/>
+
+      <button className='btn btn-primary'
+              onClick={() => setCounter((state) => ({...state, counter1: counter1 + 1}))}>
+        Counter1 __ +1
+      </button>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HooksApp } from './HooksApp';
+// import { HooksApp } from './HooksApp';
 import './index.css';
+import { CounterApp } from './01-useState/CounterApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HooksApp />
+    {/* <HooksApp /> */}
+    <CounterApp />
   </React.StrictMode>,
 );
