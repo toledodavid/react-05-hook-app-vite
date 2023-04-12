56bcf165dbdee86927bb84e3c9750962da1437a4
David Toledo <dtoledoz0207@gmail.com>
2023-04-12 12:44:11 -0600

    Create an eventListener in Message Component to test Cleanup functionality when component is unmounted and remove the eventListener

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -4,10 +4,18 @@ import { useEffect } from "react";
 export const Message = () => {
 
   useEffect(() => {
-    console.log('Message Mounted');
+    // console.log('Message Mounted');
+
+    const onMouseMove = ({x, y}) => {
+      const coords = {x, y};
+      console.log(coords);
+    }
+
+    window.addEventListener('mousemove', onMouseMove);
 
     return () => {
-      console.log('Message Unmounted');
+      // console.log('Message Unmounted');
+      window.removeEventListener('mousemove', onMouseMove);
     }
   }, []);
 
