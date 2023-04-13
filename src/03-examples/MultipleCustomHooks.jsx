432d28b2ea30bcd9ad19e2384b9c3ed16c5e4997
David Toledo <dtoledoz0207@gmail.com>
2023-04-13 13:03:32 -0600

    Show bootstrap alert for isLoading message and show the quote in the view in MultipleCustomHooks component

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -6,13 +6,38 @@ export const MultipleCustomHooks = () => {
 
   const {data, isLoading, hasError} = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/1');
 
-  console.log({data, isLoading, hasError});
+  // console.log({data, isLoading, hasError});
+
+  const {quote, author} = !!data && data[0];
 
   return (
     <>
       <h1>BreakingBad Quotes</h1>
-
       <hr />
+
+      {
+        (isLoading) ?
+          (
+            <div className='alert alert-info text-center'>
+              Loading...
+            </div>
+          )
+          :
+          (
+            <figure className='text-end'>
+              <blockquote className='blockquote'>
+                <p>{quote}</p>
+              </blockquote>
+              <figcaption className='blockquote-footer'>
+                <cite title='David'>{author}</cite>
+              </figcaption>
+            </figure>
+          )
+      }
+
+
+      <button className='btn btn-primary'>Next quote</button>
+
     </>
   );
 }
