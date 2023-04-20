bca1fb26c2fd363e99298c10f4f15353e49399d6
David Toledo <dtoledoz0207@gmail.com>
2023-04-20 11:03:19 -0600

    Create Memorize and Small Components to implement memo method from React to memorize a component and avoid rerender

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,8 @@ import './index.css';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
-import { Layout } from './05-useLayoutEffect/Layout';
+// import { Layout } from './05-useLayoutEffect/Layout';
+import { Memorize } from './06-memos/Memorize';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <FormWithCustomHook /> */}
     {/* <MultipleCustomHooks /> */}
     {/* <FocusScreen /> */}
-    <Layout />
+    {/* <Layout /> */}
+    <Memorize />
   </React.StrictMode>,
 );
