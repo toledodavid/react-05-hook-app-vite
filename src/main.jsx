9e460a3df2a761e1cebc8837a6d382ca2187e248
David Toledo <dtoledoz0207@gmail.com>
2023-04-20 11:50:30 -0600

    Create MemoHook Component to implement useMemo Hook

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,8 @@ import './index.css';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Layout } from './05-useLayoutEffect/Layout';
-import { Memorize } from './06-memos/Memorize';
+// import { Memorize } from './06-memos/Memorize';
+import { MemoHook } from './06-memos/MemoHook';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <MultipleCustomHooks /> */}
     {/* <FocusScreen /> */}
     {/* <Layout /> */}
-    <Memorize />
+    {/* <Memorize /> */}
+    <MemoHook />
   </React.StrictMode>,
 );
