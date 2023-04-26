dd53990397aea866c8e1dc87dd344ba7e314af59
David Toledo <dtoledoz0207@gmail.com>
2023-04-26 11:26:35 -0600

    Work in 07-tarea-memo to implement useCallback Hook and React.memo() to memorize

---
+++ src/07-tarea-memo/Padre.jsx
@@ -0,0 +1,37 @@
+import { Hijo } from './Hijo'
+import { useCallback, useState } from 'react';
+
+export const Padre = () => {
+
+    const numeros = [2,4,6,8,10];
+    const [valor, setValor] = useState(0);
+
+    const onIncrement = useCallback((num) => {
+      setValor((v) => v + num );
+    }, []);
+
+    // const incrementar = ( num ) => {
+    //     setValor( valor + num )
+    // }
+
+
+    return (
+        <div>
+            <h1>Padre</h1>
+            <p> Total: { valor } </p>
+
+            <hr />
+
+            {
+                numeros.map( n => (
+                    <Hijo
+                        key={ n }
+                        numero={ n }
+                        incrementar={ onIncrement }
+                    />
+                ))
+            }
+            {/* <Hijo /> */}
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,8 @@ import './index.css';
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
 // import { MemoHook } from './06-memos/MemoHook';
-import { CallbackHook } from './06-memos/CallbackHook';
+// import { CallbackHook } from './06-memos/CallbackHook';
+import { Padre } from './07-tarea-memo/Padre';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -25,6 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     {/* <Layout /> */}
     {/* <Memorize /> */}
     {/* <MemoHook /> */}
-    <CallbackHook />
+    {/* <CallbackHook /> */}
+    <Padre />
   </React.StrictMode>,
 );
