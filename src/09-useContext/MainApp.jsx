8a5045d8accc3a62177280d65ee015812b646518
David Toledo <dtoledoz0207@gmail.com>
2023-05-11 10:22:48 -0600

    Create Navbar component to implement Link from react-router-dom

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -2,12 +2,16 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { HomePage } from './HomePage';
 import { LoginPage } from './LoginPage';
 import { AboutPage } from './AboutPage';
+import { Navbar } from './Navbar';
 
 
 export const MainApp = () => {
   return(
     <>
       <h1>MainApp</h1>
+
+      <Navbar />
+
       <hr />
 
       <Routes>
