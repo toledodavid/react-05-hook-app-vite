b9a15d3fb091bd2c5b64820fd0ca227e2a277e68
David Toledo <dtoledoz0207@gmail.com>
2023-05-12 11:33:48 -0600

    Implement useContext in LoginPage and HomePage to show user information and implenent useState in UserProvider to share setUser function

--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,14 +1,19 @@
+import { useState } from 'react';
 import { UserContext } from './UserContext';
 
-const user = {
-  id: 123,
-  name: 'David Toledo',
-  email: 'david@gmail.com'
-}
+// const user = {
+//   id: 123,
+//   name: 'David Toledo',
+//   email: 'david@gmail.com'
+// }
 
 export const UserProvider = ({children}) => {
+
+  const [user, setUser] = useState();
+
   return(
-    <UserContext.Provider value={{hello: 'World', user}}>
+    // <UserContext.Provider value={{hello: 'World', user}}>
+    <UserContext.Provider value={{user, setUser}}>
       {children}
     </UserContext.Provider>
   );
