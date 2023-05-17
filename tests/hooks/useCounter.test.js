4e8b39251626d49e6fb1b8d965c42270cec233fa
David Toledo <dtoledoz0207@gmail.com>
2023-05-17 11:47:59 -0600

    Add test for increment, decrement and reset functions from useCounter custom hook

--- tests/hooks/useCounter.test.js
+++ tests/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { useCounter } from '../../src/hooks/useCounter';
 
 
@@ -19,6 +19,42 @@ describe('Tests for useCounter custom hook', () => {
     const {counter} = result.current;
 
     expect(counter).toBe(100);
+  });
+
+  test('Should increment the counter value', () => {
+    const {result} = renderHook(() => useCounter(100));
+    const {increment} = result.current;
+
+    act(() => {
+      increment();
+      increment(2);
+    });
+
+    expect(result.current.counter).toBe(103);
+  });
+
+  test('Should decrement the counter value', () => {
+    const {result} = renderHook(() => useCounter(100));
+    const {decrement} = result.current;
+
+    act(() => {
+      decrement();
+      decrement(2);
+    });
+
+    expect(result.current.counter).toBe(97);
+  });
+
+  test('Should reset value to the initial value', () => {
+    const {result} = renderHook(() => useCounter(100));
+    const {increment, reset} = result.current;
+
+    act(() => {
+      increment(2);
+      reset();
+    });
+
+    expect(result.current.counter).toBe(100);
   });
 
 });
