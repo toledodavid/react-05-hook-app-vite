6fced238059d68f2dda1836aa3de26d3575f6e83
David Toledo <dtoledoz0207@gmail.com>
2023-05-24 11:16:07 -0600

    Add tests for onToggleTodo and onDeleteTodo events from <TodoItem /> component

--- tests/08-useReducer/TodoItem.test.jsx
+++ tests/08-useReducer/TodoItem.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { TodoItem } from '../../src/08-useReducer/TodoItem';
 
 describe('Tests for <TodoItem /> Component', () => {
@@ -25,6 +25,36 @@ describe('Tests for <TodoItem /> Component', () => {
     expect(liElement.className).toBe('list-group-item d-flex justify-content-between');
     expect(spanElement.className).toContain('align-self-center');
     expect(spanElement.className).not.toContain('text-decoration-line-through');
+  });
+
+  test('Should show the Todo as completed', () => {
+    todo.done = true;
+
+    render(<TodoItem todo={todo} onToggleTodo={onToggleTodoMock} onDeleteTodo={onDeleteTodoMock} />);
+
+    const spanElement = screen.getByLabelText('span');
+
+    expect(spanElement.className).toBe('align-self-center text-decoration-line-through');
+  });
+
+  test('Should call onToggleTodo when span is clicked', () => {
+    render(<TodoItem todo={todo} onToggleTodo={onToggleTodoMock} onDeleteTodo={onDeleteTodoMock} />);
+    const spanElement = screen.getByLabelText('span');
+
+    fireEvent.click(spanElement);
+
+    expect(onToggleTodoMock).toHaveBeenCalled();
+    expect(onToggleTodoMock).toHaveBeenCalledWith(todo.id);
+  });
+
+  test('Should call onDeleteTodo when delete button is clicked', () => {
+    render(<TodoItem todo={todo} onToggleTodo={onToggleTodoMock} onDeleteTodo={onDeleteTodoMock} />);
+    const buttonElement = screen.getByRole('button');
+
+    fireEvent.click(buttonElement);
+
+    expect(onDeleteTodoMock).toHaveBeenCalled();
+    expect(onDeleteTodoMock).toHaveBeenCalledWith(todo.id);
   });
 
 });
